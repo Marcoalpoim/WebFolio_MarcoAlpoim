715cd2e2046557fefb7e4d27d2e4d89658f1dd64
Marcoalpoim <marco.alpoim@live.com.pt>
2026-01-11 19:31:31 +0000

    Fix CSS paths for GitHub Pages

--- scripts/script.js
+++ scripts/script.js
@@ -17,7 +17,7 @@ window.addEventListener("load", () => {
 
     // 🔥 intro runs immediately
     const script = document.createElement("script");
-    script.src = "../scripts/intro-js/intro2.js";
+    script.src = "/scripts/intro-js/intro2.js";
     script.defer = true;
     document.body.appendChild(script);
 
@@ -41,7 +41,7 @@ window.addEventListener("load", () => {
     // 🔥 intro starts BEFORE loader ends
     setTimeout(() => {
       const script = document.createElement("script");
-      script.src = "../scripts/intro-js/intro2.js";
+      script.src = "/scripts/intro-js/intro2.js";
       script.defer = true;
       document.body.appendChild(script);
     }, EXIT_DURATION - SCRIPT_OFFSET);
